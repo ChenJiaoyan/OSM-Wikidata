f776998bed37ec195a8b3df275beab2b84f44f61
John <John@vpn070a.rzuser.uni-heidelberg.de>
2017-03-15 08:59:25 +0100

    Initial commit

---
+++ src/main/java/HelloGeoKG.java
@@ -0,0 +1,10 @@
+/**
+ * Created by John on 3/15/17.
+ */
+public class HelloGeoKG {
+    public static void main(String args[]){
+        System.out.printf("This project is to study (1) the linkage between OSM and WikiData, " +
+                "(2) the linkage between OSM and POIs \n");
+        System.out.printf("Have Fun!");
+    }
+}
